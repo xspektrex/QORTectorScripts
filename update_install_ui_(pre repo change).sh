c14299b600079dc0da4985c4be263e26de3d05f9
xspektrex <xspektrex@gmail.com>
2022-03-13 14:01:48 -0400

    refactor: code changes
    - Additional scripting to better inform user
    - Additional scripting to prune unused nodeJS cached versions
    
    Signed-off-by: xspektrex <xspektrex@gmail.com>

--- update_install_ui_(pre repo change).sh
+++ update_install_ui_(pre repo change).sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 echo ""
-echo "Updating APT and installing curl"
+echo "Updating APT and installing curl..."
 apt update && apt install curl -y
 echo ""
 
-echo "stopping node.js if running"
+echo "stopping node.js if running..."
 killall -9 node*
 echo ""
 
-echo "Installing NPM (Node Package Manager) if not already installed"
-apt install npm
+echo "Installing NPM (Node Package Manager) if not already installed..."
+apt install npm -y
 echo ""
 
-echo "NodeJS version if installed (or to be updated)"
+echo "NodeJS version if installed (or to be updated)..."
 node -v
 echo ""
 
@@ -21,23 +21,27 @@ echo ""
 #. ~/.profile
 #echo ""
 
-echo "Clear NPM Cache"
+echo "Clear NPM Cache..."
 npm cache clean -f
 echo ""
 
-echo "Installing yarn package manager"
+echo "Installing yarn package manager..."
 npm install -g yarn
 echo ""
 
-echo "Installing Node's version manager 'n'"
+echo "Installing Node's version manager 'n'..."
 npm install -g n
 echo ""
 
-echo "Installing most recent stable node.js release"
-sudo n stable
+echo "Installing most recent stable node.js release..."
+n latest
 echo ""
 
-echo "stopping node.js if running"
+echo "Pruning previous versions of nodeJS installed by 'n'..."
+n prune
+echo ""
+
+echo "stopping node.js if running..."
 killall -9 node*
 echo ""
 
@@ -46,11 +50,10 @@ rm -R UI
 echo ""
 
 echo "cloning git repositories for Qortal UI"
-git clone https://github.com/Qortal/qortal-ui
+git clone https://github.com/qortal/UI
 echo ""
 
 cd UI
-
 cd qortal-ui
 
 echo "installing dependencies and linking with yarn link for build process"
